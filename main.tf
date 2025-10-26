57ea6c140e76eb81dabf00a5b3b6ab0be2d38e52
Snarvid82 <arvid.vik@gmail.com>
2025-10-26 06:21:22 +0100

    Default VPC created

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@
 resource "aws_s3_bucket" "s3_for_state_backend" {
   bucket = "aev-my-tf-state"
 }
+
+# Create default VPC
+resource "aws_default_vpc" "default" {
+    tags = {
+        Name = "Default VPC"
+    }
+}
