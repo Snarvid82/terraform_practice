6eb8e66e07076f708f824e85e6f96531dcc1a022
Snarvid82 <arvid.vik@gmail.com>
2025-10-27 14:31:16 +0100

    EC2 instance created for private subnet 1 and 2

--- main.tf
+++ main.tf
@@ -92,7 +92,27 @@ resource "aws_subnet" "db_private_subnet2" {
 
 
 # Create EC2 instances
+provider "aws" {
+    region = "eu-north-1"
+}
 
+resource "aws_instance" "app_subnet1" {
+    ami = "ami-0aa78f446b4499266"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.app_private_subnet1.id
+    tags = {
+        Name = "EC2 for app subnet 1"
+    }
+}
+
+resource "aws_instance" "app_subnet2" {
+    ami = "ami-0aa78f446b4499266"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.app_private_subnet2.id
+    tags = {
+        Name = "EC2 for app subnet 2"
+    }
+}
 
 # Create security groups
 
